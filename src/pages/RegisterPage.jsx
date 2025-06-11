f26700392de57a5ef3438ce5d8b1dfa84f6a631d
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-06-11 12:16:12 +0530

    register UI form data handling

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,8 +4,27 @@ import "../styles/Register.scss";
 
 const RegisterPage = () => {
   
-
+const [formData,setformData] = useState({
+  firstName:"",
+  lastName:"",
+  email:"",
+  password:"",
+  confirmPassword:"",
+  profileImage:null
+})
   
+console.log(formData);
+
+
+const handleChange = (e) =>{
+  const {name,value,files} = e.target
+  setformData({
+    ...formData,
+    [name]:[value],
+    [name]:name === "profileImage" ?files[0]:value
+
+})
+}
 
   
 
@@ -20,32 +39,38 @@ const RegisterPage = () => {
           <input
             placeholder="First Name"
             name="firstName"
-            
+            value={formData.firstName}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Last Name"
             name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Email"
             name="email"
             type="email"
-
+            value={formData.email}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Password"
             name="password"
-
+            value={formData.password}
+            onChange={handleChange}
             type="password"
             required
           />
           <input
             placeholder="Confirm Password"
             name="confirmPassword"
-
+            value={formData.confirmPassword}
+            onChange={handleChange}
             type="password"
             required
           />
@@ -57,6 +82,7 @@ const RegisterPage = () => {
             type="file"
             name="profileImage"
             accept="image/*"
+            onChange={handleChange}
             style={{ display: "none" }}
             required
           />
@@ -65,12 +91,14 @@ const RegisterPage = () => {
             <p>Upload Your Photo</p>
           </label>
 
-          
+          {formData.profileImage && (
+
             <img
               
               alt="profile photo"
               style={{ maxWidth: "80px" }}
             />
+          )}
           
           <button type="submit" >REGISTER</button>
         </form>
